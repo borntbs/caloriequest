5355c674a7bd19a29e943f002b6910e5c5e78e05
btbs <aowl10012004@gmail.com>
2023-07-19 13:56:38 +0800

    Form now retrieves user input and calls API for data

---
+++ src/DAL/GET.js
@@ -0,0 +1,17 @@
+const nutriAPIGateway =
+  "https://j8sa414f2f.execute-api.ap-southeast-2.amazonaws.com/Retrieve_Nutrition_Data";
+
+export const getNutriInfo = async (e) => {
+  const sizeSuffix = e.currentTarget.querySelector("#selectedSize").innerHTML;
+  const size = e.currentTarget.querySelector("#servingSize").value + sizeSuffix;
+  let food = e.currentTarget.querySelector("#foodItem").value;
+  food = food.split(" ").join("%20");
+
+  e.preventDefault();
+  const response = await fetch(nutriAPIGateway + `?size=${size}&food=${food}`, {
+    method: "GET",
+  });
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+};
