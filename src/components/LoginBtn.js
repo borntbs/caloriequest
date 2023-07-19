8ba3de91fbea1d1a3161200bc5bba2d44a6bbef8
btbs <aowl10012004@gmail.com>
2023-07-19 19:20:16 +0800

    Added Login Button
    Login button added with hover tooltip

---
+++ src/components/LoginBtn.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const LoginBtn = (props) => {
+  return (
+    <div className="absolute top-5 right-5 flex flex-col justify-items-center items-center gap-3">
+      <button
+        className="peer py-2 text-lg px-5 bg-amber-400 rounded-xl w-fit"
+        type="button"
+      >
+        Login
+      </button>
+      <div className="bg-amber-300 opacity-0 peer-hover:opacity-100 transition-opacity duration-500 p-3 text-md rounded-xl">
+        Login to view your profile!
+      </div>
+    </div>
+  );
+};
+
+export default LoginBtn;
